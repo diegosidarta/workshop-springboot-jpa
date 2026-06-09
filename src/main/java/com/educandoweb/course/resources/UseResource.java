d96cf042c94fda060b420ef6b0590caf4b7736cf
Diego Sidarta <diegosidarta123@gmail.com>
2026-06-09 15:57:24 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/resources/UseResource.java
+++ src/main/java/com/educandoweb/course/resources/UseResource.java
@@ -1,6 +1,6 @@
 package com.educandoweb.course.resources;
 
-import com.educandoweb.entities.User;
+import com.educandoweb.course.entities.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
